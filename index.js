83300bb40f78efba928030c9aad0b1ef8f179e95
Gabo Esquivel <contact@gaboesquivel.com>
2025-02-27 19:10:02 -0600

    content: project achievements

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-export default require('./index.json')
-export const projects = require('./projects.json')
+const index = require('./index.json');
+const projects = require('./projects.json');
+
+module.exports = index;
+module.exports.projects = projects; 
