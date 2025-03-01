4b53c363fd8bb9d0dcd247bcbbf8f0dc6ac493c6
Gabo Esquivel <contact@gaboesquivel.com>
2025-03-01 10:18:52 -0600

    feat: export api

---
+++ src/api.ts
@@ -0,0 +1,17 @@
+import { projects } from './projects';
+import { techStack } from './tech';
+
+export function getProjectBySlug(slug: string) {
+    return projects.find((project) => project.slug === slug);
+}
+
+export function getTechStackByTag(tag: string) {
+    return {
+        ...techStack.find((tech) => tech.tag === tag), 
+        projects: getProjectsByTechnology(tag)
+    }
+}
+
+export function getProjectsByTechnology(tag: string) {
+    return projects.filter((project) => project.technologies.includes(tag));
+}
