a061ab1148a7b20f2f99adaee8eebd4c9d38ae34
Gabo Esquivel <contact@gaboesquivel.com>
2025-03-01 10:37:58 -0600

    chore: update api

--- src/api.ts
+++ src/api.ts
@@ -16,6 +16,6 @@ export function getProjectsByTechnology(tag: string) {
     return projects.filter((project) => project.technologies.includes(tag));
 }
 
-export function gaboLog( ) {
+export function gaboLogData() {
     console.log({project: projects, tech: techStack});
 }
