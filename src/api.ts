7a6df3aa5f13cd65718aa71987c1a3448c025411
Gabo Esquivel <contact@gaboesquivel.com>
2025-03-23 16:24:46 -0600

    feat: add getProjectsByTechnologySlug

--- src/api.ts
+++ src/api.ts
@@ -1,35 +1,41 @@
-import { projects } from './projects'
-import { techStack } from './tech'
-import type { Tag, TechStackItemWithProjects } from './types'
+import { projects } from "./projects";
+import { techStack } from "./tech";
+import type { Tag, TechStackItemWithProjects } from "./types";
 
 export function getProjectBySlug(slug: string) {
-  return projects.find((project) => project.slug === slug)
+	return projects.find((project) => project.slug === slug);
 }
 
 export function getTechStackByTag(tag: Tag): TechStackItemWithProjects | null {
-  const tech = techStack.find((tech) => tech.tag === tag)
-  if (!tech) return null
-  return {
-    ...tech,
-    projects: getProjectsByTechnology(tag),
-  }
+	const tech = techStack.find((tech) => tech.tag === tag);
+	if (!tech) return null;
+	return {
+		...tech,
+		projects: getProjectsByTechnology(tag),
+	};
 }
 
 export function getTechStackBySlug(
-  slug: string,
+	slug: string,
 ): TechStackItemWithProjects | null {
-  const tech = techStack.find((tech) => tech.slug === slug)
-  if (!tech) return null
-  return {
-    ...tech,
-    projects: getProjectsByTechnology(tech.tag),
-  }
+	const tech = techStack.find((tech) => tech.slug === slug);
+	if (!tech) return null;
+	return {
+		...tech,
+		projects: getProjectsByTechnology(tech.tag),
+	};
 }
 
 export function getProjectsByTechnology(tag: Tag) {
-  return projects.filter((project) => project.tech.includes(tag))
+	return projects.filter((project) => project.tech.includes(tag));
+}
+
+export function getProjectsByTechnologySlug(slug: string) {
+	const tech = techStack.find((tech) => tech.slug === slug);
+	if (!tech) return [];
+	return projects.filter((project) => project.tech.includes(tech.tag));
 }
 
 export function gaboLog() {
-  console.log({ projects, tech: techStack })
+	console.log({ projects, tech: techStack });
 }
